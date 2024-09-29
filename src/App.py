10f0d8e7f27db31622d70451883b4c82eb2efe6f
anerie <141164361+anerie-anerie@users.noreply.github.com>
2024-09-29 00:03:21 -0400

    Update App.py

--- src/App.py
+++ src/App.py
@@ -1,1 +1,28 @@
-#random
+try:
+    import streamlit as st
+    import pandas as pd
+    import os
+    import sys
+    from io import BytesIO, StringIO
+
+except Exception as e:
+    print("Some Modules are Missing: {}".format(e))
+
+def main():
+    st.info(__doc__)
+    st.markdown(STYLE, unsafe_allow_html = True)
+    file = st.file_uploader(label = "Uplooad your file here: ", type = ["png","jpg"])
+    show_file = st.empty()
+
+    if not file:
+        show_file.info("Please upload a file: {}".format("".join(["png","jpg"])))
+        return
+
+    content = file.getvalue()
+
+    if isistance(file, BytesIO):
+        show_file.image(file)
+    file.close()
+
+
+main()
