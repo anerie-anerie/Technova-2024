cb1e3d499fbaaa8bc43b223e766b60dcaea29686
Tishya-2008 <141259871+Tishya-2008@users.noreply.github.com>
2024-09-29 08:43:26 -0400

    Update fileO.py

--- backend/fileO.py
+++ backend/fileO.py
@@ -2,9 +2,7 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
-print('hi')
 uploaded_file = st.file_uploader("Choose a file")
-print('hi')
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
